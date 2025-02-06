59297836b624d34c52654f38e4b7a1f5b3f77760
chuchen0210 <167320956+chuchen0210@users.noreply.github.com>
2025-02-06 22:52:58 +0800

    Delete build directory

--- build/image/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hitcrt/catkin_ws/src/image
-# Build directory: /home/hitcrt/catkin_ws/build/image
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
